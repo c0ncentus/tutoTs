359a66fb3e6e3259710c7baa94e5e06d51f591c3
c0ncentus <jeremymartin33610@gmail.com>
2023-06-16 13:53:09 +0200

    up

--- src/1-BASICS/3_operator.ts
+++ src/1-BASICS/3_operator.ts
@@ -1,7 +1,7 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
 
 
-// comparators
+// BOOLEAN
 console.log(`(4 > 4) === false: ${(4 > 4) === false}`)
 console.log(`(4 >= 4) === true: ${(4 >= 4) === true}`)
 console.log(`(4 < 4) === false: ${(4 < 4) === false}`)
@@ -19,9 +19,9 @@ console.log(`(true || true) === true: ${(true || true) === true}`)
 console.log(`(true || false) === true: ${(true || false) === true}`)
 console.log(`(true && false) === false: ${(true && false) === false}`)
 console.log(`(true && true) === true: ${(true && true) === true}`)
-// function toto(x: string) { return x !== "" };
 
-// nums operator
+
+// NUMBER
 console.log(`3 + 2: ${3 + 2}`)
 console.log(`3 * 2: ${3 * 2}`)
 console.log(`3 ** 2: ${3 ** 2}`)
@@ -38,6 +38,24 @@ console.log(`null ??"is nullish": ${null ?? "is nullish"}`)
 console.log(`undefined ??"is nullish": ${undefined ?? "is nullish"}`)
 console.log(`0 ??"is not nullish": ${0 ?? "is not nullish"}`)
 
+
+
+// STRING
+console.log(`"3"+"0": ${"3" + "0"}`)
+console.log(`"3"+"0": ${"3" + "0"}`)
+
+
+console.log("Fr100".length)
+console.log("Fr100".repeat(2))
+console.log("Fr100".replace("0", "_"))
+console.log("Fr100".search("F"))
+console.log("Fr100".split(""))
+console.log("Fr100".split("1"))
+console.log("Fr100".substring(0, 3))
+console.log("Fr100".toUpperCase())
+console.log("Fr100".toLowerCase())
+console.log("  Fr100  ".trim())
+console.log(`"3"+"0": ${"3" + "0"}`)
 
 
 // === falsy value ===
